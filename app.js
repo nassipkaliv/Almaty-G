5cdd5795e638107868091e6cfc34b224d04ef366
nassipkaliv <nassipkaliv@gmail.com>
2023-10-30 12:50:34 +0600

    modal + lazyload for img carousel

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var images = document.querySelectorAll("img:not([loading])");
+  
+  images.forEach(function(image) {
+      image.setAttribute("loading", "lazy");
+  });
+});
