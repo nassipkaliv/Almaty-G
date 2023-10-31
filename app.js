de0f588462a94febdae20e4aa5e1bd2154749486
nassipkaliv <nassipkaliv@gmail.com>
2023-11-01 00:33:25 +0600

    logo svg fix!

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+$('#myCarousel').on('slide.bs.carousel', function (e) {
+  var items = e.relatedTarget.getElementsByClassName("img-thumbnail");
+  for (var i = 0; i < items.length; i++) {
+      items[i].style.opacity = "0.5";
+  }
+  items[1].style.opacity = "1";
+});
