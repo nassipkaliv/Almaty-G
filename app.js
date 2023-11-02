20595fad042c8c52947aee7b6e6ce83191a29ebc
nassipkaliv <nassipkaliv@gmail.com>
2023-11-02 13:58:18 +0600

    fix of everything!

--- app.js
+++ app.js
@@ -6,10 +6,31 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-$('#myCarousel').on('slide.bs.carousel', function (e) {
-  var items = e.relatedTarget.getElementsByClassName("img-thumbnail");
-  for (var i = 0; i < items.length; i++) {
-      items[i].style.opacity = "0.5";
-  }
-  items[1].style.opacity = "1";
+const toggleButtons = document.querySelectorAll('.service-more');
+
+toggleButtons.forEach((button) => {
+  const targetId = button.getAttribute('data-bs-target');
+  const toggleText = button.querySelector('.toggle-text');
+  const additionalServices = document.querySelector(targetId);
+
+  additionalServices.addEventListener('show.bs.collapse', function () {
+    toggleText.textContent = 'Скрыть';
+  });
+
+  additionalServices.addEventListener('hide.bs.collapse', function () {
+    toggleText.textContent = 'еще 4 услуги';
+  });
 });
+
+function changeActive(index) {
+  const smallImages = document.querySelectorAll('.img-thumbnail');
+  smallImages.forEach((image, i) => {
+      if (i === index) {
+          image.classList.add('active');
+          image.setAttribute('aria-selected', 'true');
+      } else {
+          image.classList.remove('active');
+          image.setAttribute('aria-selected', 'false');
+      }
+  });
+}
